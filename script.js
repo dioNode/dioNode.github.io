1ba09950458677868926f5c6e8c0bdcc2d5cf305
dioNode <dion_lao@hotmail.com>
2017-11-13 22:04:35 +1000

    Added subsection links

--- script.js
+++ script.js
@@ -21,6 +21,7 @@ var i = 0;
 var counter = 0;
 var requestID;
 var toggleSpeed = 500;
+var expandDistance = 200;
 
 $(document).ready(function() {
 
@@ -86,16 +87,9 @@ function swoop_symbol(divtext, text, symbol, frameRate, infinite) {
 }
 
 function init_hovers() {
+	$( "#subheading-wrapper .left .text" ).animate({fontSize:"toggle"}, toggleSpeed);
 	$( "#subheading-wrapper .right .text" ).animate({fontSize:"toggle"}, toggleSpeed);
-	$( "#subheading-wrapper .right" ).hover(
-		function() {
-			$( "#subheading-wrapper .right .text" ).animate({fontSize:"toggle"}, toggleSpeed);
-			$("#subheading-wrapper .cogwheel").addClass("spin");
-	  	}, function() {
-	    	$( "#subheading-wrapper .right .text" ).animate({fontSize:"toggle"}, toggleSpeed);
-	    	$("#subheading-wrapper .cogwheel").removeClass("spin");
-	  }
-	);
+
 
 	$("#title .text").mouseover(function() {
 		//i = 0;
@@ -104,4 +98,57 @@ function init_hovers() {
 		if (i==0)
 			swoop_symbol("#title h2", "Information Technology and Mechatronics Engineering", "&nbsp &nbsp", 1, false);
 	})
+
+
+	$( "#subheading-wrapper .right" ).hover(
+		function() {
+			$("#subheading-wrapper .right").addClass("highlighted");
+			$("#subheading-wrapper .right .text" ).stop( true, true ).animate({fontSize:"toggle"}, toggleSpeed);
+			$("#subheading-wrapper .right .upper.cogwheel").addClass("spin").stop( true, true ).animate({top:"-="+expandDistance+"px"}, toggleSpeed);
+			$("#subheading-wrapper .right .lower.cogwheel").addClass("spin").stop( true, true ).animate({top:"+="+expandDistance+"px"}, toggleSpeed);
+			$("#subheading-wrapper .right .orb").stop( true, true ).animate({height: ""+expandDistance*2+"px", top: "-="+expandDistance+"px"}, toggleSpeed);
+	  	}, function() {
+	    	$("#subheading-wrapper .right .text" ).stop( true, true ).animate({fontSize:"toggle"}, toggleSpeed);
+	    	$("#subheading-wrapper .right .upper.cogwheel").removeClass("spin").stop( true, true ).animate({top:"+="+expandDistance+"px"}, toggleSpeed);
+	    	$("#subheading-wrapper .right .lower.cogwheel").removeClass("spin").stop( true, true ).animate({top:"-="+expandDistance+"px"}, toggleSpeed);
+	    	$("#subheading-wrapper .right .orb").stop( true, true ).animate({height: "10px", top: "+="+expandDistance+"px"}, toggleSpeed);
+	    	$("#subheading-wrapper .right").removeClass("highlighted");
+	  }
+	);
+
+
+	$( "#subheading-wrapper .left" ).hover(
+		function() {
+			$("#subheading-wrapper .left").addClass("highlighted");
+			$("#subheading-wrapper .left .text" ).stop( true, true ).animate({fontSize:"toggle"}, toggleSpeed);
+			$("#subheading-wrapper .left .upper.cogwheel").addClass("spin").stop( true, true ).animate({top:"-="+expandDistance+"px"}, toggleSpeed);
+			$("#subheading-wrapper .left .lower.cogwheel").addClass("spin").stop( true, true ).animate({top:"+="+expandDistance+"px"}, toggleSpeed);
+			$("#subheading-wrapper .left .orb").stop( true, true ).animate({height: ""+expandDistance*2+"px", top: "-="+expandDistance+"px"}, toggleSpeed);
+	  	}, function() {
+	    	$("#subheading-wrapper .left .text" ).stop( true, true ).animate({fontSize:"toggle"}, toggleSpeed);
+	    	$("#subheading-wrapper .left .upper.cogwheel").removeClass("spin").stop( true, true ).animate({top:"+="+expandDistance+"px"}, toggleSpeed);
+	    	$("#subheading-wrapper .left .lower.cogwheel").removeClass("spin").stop( true, true ).animate({top:"-="+expandDistance+"px"}, toggleSpeed);
+	    	$("#subheading-wrapper .left .orb").stop( true, true ).animate({height: "10px", top: "+="+expandDistance+"px"}, toggleSpeed);
+	    	$("#subheading-wrapper .left").removeClass("highlighted");
+	  }
+	);
+
+	
+/*	$( "#subheading-wrapper .top" ).hover(
+		function() {
+			$("#subheading-wrapper .top").addClass("highlighted");
+			$("#subheading-wrapper .top .text" ).stop( true, true ).animate({fontSize:"toggle"}, toggleSpeed);
+			$("#subheading-wrapper .top .lefter.cogwheel").addClass("spin").stop( true, true ).animate({left:"-="+expandDistance+"px"}, toggleSpeed);
+			$("#subheading-wrapper .top .righter.cogwheel").addClass("spin").stop( true, true ).animate({left:"+="+expandDistance+"px"}, toggleSpeed);
+			$("#subheading-wrapper .top .orb").stop( true, true ).animate({width: ""+expandDistance*2+"px", left: "-="+expandDistance+"px"}, toggleSpeed);
+	  	}, function() {
+	    	$("#subheading-wrapper .top .text" ).stop( true, true ).animate({fontSize:"toggle"}, toggleSpeed);
+	    	$("#subheading-wrapper .top .lefter.cogwheel").removeClass("spin").stop( true, true ).animate({left:"+="+expandDistance+"px"}, toggleSpeed);
+	    	$("#subheading-wrapper .top .righter.cogwheel").removeClass("spin").stop( true, true ).animate({left:"-="+expandDistance+"px"}, toggleSpeed);
+	    	$("#subheading-wrapper .top .orb").stop( true, true ).animate({width: "10px", left: "+="+expandDistance+"px"}, toggleSpeed);
+	    	$("#subheading-wrapper .top").removeClass("highlighted");
+	  }
+	);*/
+
+	
 }
