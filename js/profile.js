cfc160a5b88c9b1ff4d7a37cf7ac51d934c5b0cc
dioNode <dion_lao@hotmail.com>
2017-11-15 08:08:24 +1000

    Added scroll library and functionality

--- js/profile.js
+++ js/profile.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
 	$("body").children().hide();
 	$("body").children().fadeIn(2000);
 
+	AOS.init({
+      offset: 200,
+      duration: 1000,
+      easing: 'ease-in-sine',
+      delay: 100,
+    });
 });	
