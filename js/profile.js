45751a11bad0a516e064abf50c6df2f8abd92012
Dion Lao <dion.lao@uqconnect.edu.au>
2017-11-15 17:33:08 +1000

    Added expanding timeline functionality

--- js/profile.js
+++ js/profile.js
@@ -16,7 +16,29 @@ $(document).ready(function() {
 
 	storeDates();
 	arrangeExperiences();
+
+	$("#experiences .exp").click(expandExperience);
 });
+
+function expandExperience() {
+	hideExperiences();
+	$(this).addClass("expanded").children().show();
+	$(this).animate({
+		width: $(this).get(0).scrollWidth,
+		height: $(this).get(0).scrollHeight,
+		opacity: 0.9
+	}, 400);
+
+}
+
+function hideExperiences() {
+	$("#experiences .exp").children().fadeOut();
+	$("#experiences .exp").animate({
+		width: "30px",
+		height:"30px",
+		opacity: 1
+	},200)
+}
 
 function storeDates() {
 	min = null;
@@ -35,5 +57,8 @@ function arrangeExperiences() {
 	for (var date in experiences) {
 		var percentage = 100*(date-min)/(max-min);
 		$(experiences[date]).css("top",percentage+"%");
+		var imgurl = $(experiences[date]).find("img").attr("src");
+		console.log(imgurl);
+		$(experiences[date]).css("background-image", 'url("'+imgurl+'")');
 	}
 }
