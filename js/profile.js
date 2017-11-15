b7a7254cc21b0b378a011f4b77004220478ce6c0
Dion Lao <dion.lao@uqconnect.edu.au>
2017-11-15 15:51:17 +1000

    Added date rearranging calculations for timeline

--- js/profile.js
+++ js/profile.js
@@ -1,11 +1,39 @@
+var experiences = {};
+var min;
+var max;
+
 $(document).ready(function() {
 	$("body").children().hide();
 	$("body").children().fadeIn(2000);
+	$("#experiences .exp").children().hide();
 
 	AOS.init({
-      offset: 200,
-      duration: 1000,
-      easing: 'ease-in-sine',
-      delay: 100,
-    });
-});	
+    offset: 200,
+    duration: 1000,
+    easing: 'ease-in-sine',
+    delay: 100,
+  });
+
+	storeDates();
+	arrangeExperiences();
+});
+
+function storeDates() {
+	min = null;
+	max = null;
+	$( "#experiences .exp" ).each(function( index ) {
+		var tag = $(this).attr("tag");
+		experiences[tag] = $(this);
+		if (min==null || tag < min)
+			min = tag;
+		if (max==null || tag > max)
+			max = tag;
+	});
+}
+
+function arrangeExperiences() {
+	for (var date in experiences) {
+		var percentage = 100*(date-min)/(max-min);
+		$(experiences[date]).css("top",percentage+"%");
+	}
+}
