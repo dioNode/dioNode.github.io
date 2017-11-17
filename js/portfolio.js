2a7970f6f278ef2052f84ca9b7d24fc998c6bcaa
Dion Lao <dion.lao@uqconnect.edu.au>
2017-11-17 13:06:03 +1000

    Added some more portolios and fixed up formatting

--- js/portfolio.js
+++ js/portfolio.js
@@ -3,4 +3,13 @@ $(document).ready(function() {
 	$("body").children().fadeIn(2000);
 
 	init_home_interaction();
+	init_portfolio_hovers();
 });
+
+function init_portfolio_hovers() {
+	$("#portfolios .work-container").hover(function(){
+		$(this).addClass("bulge");
+	}, function(){
+		$(this).removeClass("bulge");
+	})
+}
