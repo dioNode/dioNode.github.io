63c223d39881f6ccd62ccbef5b2eabfe4b43ae75
dioNode <dion_lao@hotmail.com>
2017-11-22 22:42:56 +1000

    Added float-js files

--- js/profile.js
+++ js/profile.js
@@ -20,6 +20,17 @@ $(document).ready(function() {
 	init_expand_interactions();
 	init_home_interaction();
 	init_skills_interaction();
+
+
+	floatJS.init({
+	 
+	  'deceleration':   0.5, // Defines animation deceleration rate (default: 0.5). 
+	  'strength':       0.5, // Defines how much layers are offset (default: 0.5). 
+	  'precision':        1, // Decimal points for transform values (default: 1). 
+	  'direction':       -1  // 1 = linear, -1 = natural / inverted (default: -1). 
+	 
+	})
+	
 });
 
 function init_skills_interaction() {
