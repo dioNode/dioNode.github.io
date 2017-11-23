cb39442fc364b211638f0414d5fa47b4eed84dd5
dioNode <dion_lao@hotmail.com>
2017-11-23 18:30:06 +1000

    Fixed up libraries and folders

--- js/profile.js
+++ js/profile.js
@@ -7,12 +7,7 @@ $(document).ready(function() {
 	$("body").children().fadeIn(2000);
 	$("#experiences .exp").children().hide();
 
-	AOS.init({
-	    offset: 200,
-	    duration: 1000,
-	    easing: 'ease-in-sine',
-	    delay: 0,
- 	 });
+	init_scroll_fades();
 
 	storeDates();
 	arrangeExperiences();
@@ -21,17 +16,18 @@ $(document).ready(function() {
 	init_home_interaction();
 	init_skills_interaction();
 
-
-	floatJS.init({
-	 
-	  'deceleration':   0.5, // Defines animation deceleration rate (default: 0.5). 
-	  'strength':       0.5, // Defines how much layers are offset (default: 0.5). 
-	  'precision':        1, // Decimal points for transform values (default: 1). 
-	  'direction':       -1  // 1 = linear, -1 = natural / inverted (default: -1). 
-	 
-	})
 	
 });
+
+function init_scroll_fades() {
+	AOS.init({
+	    offset: 200,
+	    duration: 1000,
+	    easing: 'ease-in-sine',
+	    delay: 0,
+ 	 });
+
+}
 
 function init_skills_interaction() {
 	$("#skills .skill").hover(function(){
@@ -80,7 +76,6 @@ function expandExperience() {
 }
 
 function hideExperiences() {
-	console.log(this.defaultPrevented);
 	for (var date in experiences) {
 		hideExperience(experiences[date]);
 	}
